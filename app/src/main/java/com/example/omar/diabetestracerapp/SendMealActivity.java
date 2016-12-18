ee38c2864374991252a551c39568d4baf966c7a2
PedroStamm <pedro_7915@hotmail.com>
2016-12-18 20:06:08 +0000

    [CREATE] Created SendMealActivity and added string values

---
+++ app/src/main/java/com/example/omar/diabetestracerapp/SendMealActivity.java
@@ -0,0 +1,13 @@
+package com.example.omar.diabetestracerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendMealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_meal);
+    }
+}
