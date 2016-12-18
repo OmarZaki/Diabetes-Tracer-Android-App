47b9aa66c39d3ea3c10d505de9106fc3dcd3ae6a
Omar Zaki Asaad <ozaki@student.dei.uc.pt>
2016-12-18 14:17:33 +0000

    [ADD] sendInsulinDose layout [CHANGE] button to redirect the send insulin dose  activity

--- app/src/main/java/com/example/omar/diabetestracerapp/ActivityMain.java
+++ app/src/main/java/com/example/omar/diabetestracerapp/ActivityMain.java
@@ -1,5 +1,6 @@
 package com.example.omar.diabetestracerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,10 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class ActivityMain extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    Button btnTest ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +42,16 @@ public class ActivityMain extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        btnTest =(Button)findViewById(R.id.btnTest);
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(),SendInsulinDoseActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
     @Override
